e8bf3c4815e07369db487c51611e4b869c66a0ff
legend <legendsky@hotmail.com>
2024-07-14 20:44:35 +0800

    add gemma2-9b-it

--- options.js
+++ options.js
@@ -22,7 +22,8 @@ const models = {
         "mixtral-8x7b-32768",
         "llama3-8b-8192",
         "llama3-70b-8192",
-        "gemma-7b-it"
+        "gemma-7b-it",
+        "gemma2-9b-it"
     ]
 };
 
